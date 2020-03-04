10831ae8458902aa499e41f79e90bd9d514484a7
Neuronalys <neuronalys@neuronalys.ai>
2020-03-04 17:36:29 +0100

    Enable CUDNN_HALF

--- darknet.py
+++ darknet.py
@@ -121,7 +121,7 @@ if os.name == "nt":
             lib = CDLL(winGPUdll, RTLD_GLOBAL)
             print("Environment variables indicated a CPU run, but we didn't find `"+winNoGPUdll+"`. Trying a GPU run anyway.")
 else:
-    lib = CDLL("/home/neuronalys/Projects/AlexeyAB/darknet/libdarknet.so", RTLD_GLOBAL)
+    lib = CDLL("/home/neuronalys/Projects/neuronalys-darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
